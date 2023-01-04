0e8cc3e8c65f769ad1c9e7665a7c1b36564d488e
Noor Mohammad <noormohammad.net1503011@gmail.com>
2023-01-05 00:24:03 +0600

    'role'

--- models/userModel.js
+++ models/userModel.js
@@ -24,7 +24,7 @@ const userSchema = mongoose.Schema(
     salt: String,
     role: {
       type: String,
-      enum: ['user', 'subscriber'],
+      enum: ['admin', 'subscriber'],
       default: 'subscriber',
     },
     resetPasswordLink: {
